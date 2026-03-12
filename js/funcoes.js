b7d07494aa883b50ecb1869671bd87d6430f1964
Ricardo Rodrigues <ricardoprdecarvalho@gmail.com>
2026-03-11 21:23:44 -0300

    Frameworks basicas e aula 2

---
+++ js/funcoes.js
@@ -0,0 +1,17 @@
+/*
+function minhaPrimeirafuncao(){
+  $('#area-01').css({
+    color: "#ff0000",
+    textTransform: "uppercase",
+    width: "70%"
+  });
+}
+
+function minhaSegundaFuncao(){
+  $('#area-02').empty({
+    var aulnos = ['Aluno 1', 'Aluno 2', 'Aluno 3', 'Aluno 4']
+
+    S.each(alunos, function (index, value))
+  });
+}
+*/
